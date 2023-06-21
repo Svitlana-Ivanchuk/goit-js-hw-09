e5af18dda8e7eeafcfd49cd2fd3220e3ac6fc7e6
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-06-21 15:09:18 +0300

    add js-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+btnStart.addEventListener('click', throttle(handleBtnStartClick, 1000));
+btnStop.addEventListener('click', hanleBtnStopClick);
+
+function handleBtnStartClick(evt) {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (btnStart.toggleAttribute('disabled')) {
+    btnStop.removeAttribute('disabled');
+  }
+  console.log('click');
+}
+
+function hanleBtnStopClick(evt) {
+  if (btnStop.toggleAttribute('disabled')) {
+    btnStart.removeAttribute('disabled');
+  }
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
