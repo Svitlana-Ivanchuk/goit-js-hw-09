ae7f9861fbcb808484bf7cbddb5f091167e1b194
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-06-25 23:32:35 +0300

    02 add notiflix, btn attribute

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,6 +4,10 @@ import Notiflix from 'notiflix';
 
 const inputDate = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('button');
+const timerEl = document.querySelector('.timer');
+const fieldEl = document.querySelector('.field');
+
+btnStart.setAttribute('disabled', '');
 
 const flatpickr = require('flatpickr');
 const options = {
@@ -12,16 +16,18 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates > defaultDate) {
-      btnStart.removeAttribute('disabled', '');
+    console.log(selectedDates[0]);
+
+    if (selectedDates[0] >= Date.now()) {
+      btnStart.disabled = false;
+    } else {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      btnStart.disabled = true;
     }
   },
 };
 
-console.log(Date.now());
-
 flatpickr(inputDate, options);
-btnStart.setAttribute('disabled', '');
 
 btnStart.addEventListener('click', handleBtnStartClick);
 
