0cca4829d6e75792a17427d0ba2016b003e6b2b4
Svitlana-Ivanchuk <124462920+Svitlana-Ivanchuk@users.noreply.github.com>
2023-06-27 21:04:51 +0000

    Deploying to gh-pages from @ Svitlana-Ivanchuk/goit-js-hw-09@790c2465be33a32bec5ed0e11001b6bedc9b4184 🚀

--- 03-promises.293cd68e.js
+++
@@ -1,2 +0,0 @@
-(void 0).then(function(o){var c=o.position,n=o.delay;console.log("✅ Fulfilled promise ".concat(c," in ").concat(n,"ms"))}).catch(function(o){var c=o.position,n=o.delay;console.log("❌ Rejected promise ".concat(c," in ").concat(n,"ms"))});
-//# sourceMappingURL=03-promises.293cd68e.js.map
